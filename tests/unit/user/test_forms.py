b8687200aa75fa156d58787cd414fd59d218f537
scbtm <128545782+scbtm@users.noreply.github.com>
2023-06-16 18:02:50 +0000

    task1-2 done

--- tests/unit/user/test_forms.py
+++ tests/unit/user/test_forms.py
@@ -129,12 +129,12 @@ class TestChangePasswordForm(object):
 
 class TestChangeUserDetailsForm(object):
     def test_transforms_to_expected_change_object(self):
-        # CoRise TODO: fix this this test so that it passes.
+        # CoRise TODO: (DONE) fix this this test so that it passes.
         # Hint: does the format of the data look right to you?
         data = MultiDict(
             dict(
                 submit=True,
-                birthday="25 06 2000",
+                birthday="2000-06-25",
                 gender="awesome",
                 location="here",
                 website="http://flaskbb.org",
@@ -175,7 +175,7 @@ class TestChangeUserDetailsForm(object):
         # CoRise TODO: fix this this test so that it passes.
         data = dict(
             submit=True,
-            birthday="25 06 2000",
+            birthday="2000-06-25",
             gender="awesome",
             location="here",
             website="http://flaskbb.org",
@@ -198,7 +198,7 @@ class TestChangeUserDetailsForm(object):
     def test_invalid_inputs(self, formdata):
         data = dict(
             submit=True,
-            birthday="25 06 2000",
+            birthday="2000-06-25",
             gender="awesome",
             location="here",
             website="http://flaskbb.org",
